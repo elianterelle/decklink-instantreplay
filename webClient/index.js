a85f731fa1a1cabdeee87b8067ace137aebb2d01
Elian Terelle <mail@elianterelle.de>
2021-01-30 14:01:10 +0100

    Html Stinger hinzufügen

--- webClient/index.js
+++ webClient/index.js
@@ -1,9 +1,4 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
+app.use('/', express.static('public'))
 app.listen(8080, '0.0.0.0');
