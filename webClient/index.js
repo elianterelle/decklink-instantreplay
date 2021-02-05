3acd1ccce49ba5e5515ae4ef75c252023803c6eb
Elian Terelle <elianterelle@web.de>
2021-02-05 10:09:59 +0100

    Fixes für Stinger

--- webClient/index.js
+++ webClient/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.use('/', express.static('public'))
+app.use('/', express.static(__dirname + '/public'));
 app.listen(8080, '0.0.0.0');
